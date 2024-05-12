c19fddfa8ac1f32d1afef646b0cc8f7878a78cba
Your Name <you@example.com>
2024-05-12 02:50:07 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -50,97 +50,116 @@ TreeMap * createTreeMap(int (*lower_than) (void* key1, void* key2)) {
 
 
 void insertTreeMap(TreeMap * tree, void* key, void * value) {
-    if (tree == NULL || key == NULL || value == NULL){
-        return;
-    }
-    TreeNode * newNode = createTreeNode(key, value);
-    if (tree->root == NULL){
+    if (tree == NULL || key == NULL || value == NULL) return;
+
+    if(tree->root == NULL){
+        TreeNode * newNode = createTreeNode(key, value);
         tree->root = newNode;
         tree->current = newNode;
         return;
     }
 
-    TreeNode * current = tree->root;
-    TreeNode * parent = NULL;
-    while (current != NULL){
-        if (is_equal(tree, key, current->pair->key)){
+    TreeNode *current = tree->root;
+    TreeNode *parent = NULL;
+    int izquierda = 0;
+
+    while(current != NULL){
+        if(is_equal(tree, key, current->pair->key)){
             return;
         }
-        parent = current;
-        if (tree->lower_than(key, current->pair->key)){
-            current = current->left;
-        }
-        else{
-            current = current->right;
-        }
+
+    parent = current;
+    current = tree->lower_than(key, current->pair->key) ? (izquierda = 1, current->left) : (izquierda = 0, current->right); //se pueden colocar ambos resultados para el if y el else con el formato condicion ? (si se cumple(accion 1, accion 2 que ademas se aplicara a la igualdad)) : (si no se cumple(accion1, accion2 que adenas...)))
+
     }
-    if (tree->lower_than(key, parent->pair->key)){
+
+    TreeNode * newNode = createTreeNode(key, value);
+    newNode->parent = parent;
+
+    if(izquierda){
         parent->left = newNode;
     }
     else{
         parent->right = newNode;
     }
+
+    tree->current = newNode;
 }
 
+
 TreeNode * minimum(TreeNode * x){
-    while (x->left != NULL){
+    while(x->left != NULL){
         x = x->left;
     }
     return x;
 }
 
-
 void removeNode(TreeMap * tree, TreeNode* node) {
-    if (tree == NULL || node == NULL){
-        return;
+    if (tree == NULL || node == NULL) return;
+
+    if(node->left == NULL && node->right == NULL){
+
+        if(node->parent->left == node) node->parent->left = NULL;
+        else node->parent->right = NULL;
     }
-    if (node->left == NULL && node->right == NULL){
-        if (node == tree->root){
-            tree->root = NULL;
+
+    else if(node->left != NULL && node->right == NULL){
+        if (node->parent->left == node)
+        {
+            node->parent->left = node->left;
+            node->left->parent = node->parent;
         }
-        else{
-            if (node->parent->left == node){
-                node->parent->left = NULL;
-            }
-            else{
-                node->parent->right = NULL;
-            }
+        if (node->parent->right == node)
+        {
+            node->parent->right = node->left;
+            node->left->parent = node->parent;
         }
-        free(node->pair->key);
     }
-    
+    else if(node->left == NULL && node->right != NULL){
+        if (node->parent->left == node)
+        {
+            node->parent->left = node->right;
+            node->right->parent = node->parent;
+        }
+        if (node->parent->right == node)
+        {
+            node->parent->right = node->right;
+            node->right->parent = node->parent;
+        }
+    }
+
+    else{
+        TreeNode *aux = minimum(node->right);
+        node->pair->key = aux->pair->key;
+        node->pair->value = aux->pair->value;
+        removeNode(tree, aux);
+    }
 }
 
 void eraseTreeMap(TreeMap * tree, void* key){
-    if (tree == NULL || tree->root == NULL) return;
+/*    if (tree == NULL || tree->root == NULL) return;
 
     if (searchTreeMap(tree, key) == NULL) return;
     TreeNode* node = tree->current;
     removeNode(tree, node);
-
+*/
 }
 
 
 
 
 Pair * searchTreeMap(TreeMap * tree, void* key) {
-    if (tree == NULL || tree->root == NULL || key == NULL) return NULL;
-    TreeNode* current = tree->root;
-    while (current != NULL){
-        if (is_equal(tree,key,current->pair->key)){
+    if(tree == NULL || tree->root == NULL || key == NULL) return NULL;
+    TreeNode *current = tree->root;
+    while(current != NULL){
+        if(is_equal(tree, key, current->pair->key)){
             tree->current = current;
             return current->pair;
         }
-        else{
-            if (tree->lower_than(key,current->pair->key)){
-                current = current->left;
-            }
-            else{
-                current = current->right;
-            }
-        }
+        //if(tree->lower_than(key, current->pair->key) == 1)
+        current = tree->lower_than(key, current->pair->key) ? current->left : current->right;//el operador ternario es una forma de escribir if else en una sola linea, funciona colocando la condicion ? opcion1 : opcion2. en este caso se usa para darle un valor al current, pero no necesariamente tiene que ser la igualdad de algo.
     }
-    return NULL;
+return NULL;
 }
 
 
@@ -149,11 +168,7 @@ Pair * upperBound(TreeMap * tree, void* key) {
 }
 
 Pair * firstTreeMap(TreeMap * tree) {
-    if (tree == NULL || tree->root == NULL) return NULL;
-    TreeNode* current = minimum(tree->root);
-    tree->current = current;
-    return current->pair;
-
+    return NULL;
 }
 
 Pair * nextTreeMap(TreeMap * tree) {
